7bc43690b8e5347133723494651587932b912a0a
jjohare <jjohare@users.noreply.github.com>
2026-03-03 18:47:58 +0000

    Deploy Logseq SPA @ 22e453dafccadf041315ca045774eedbd0f839ec jjohare/logseq@22e453dafccadf041315ca045774eedbd0f839ec

--- notes/dark-theme.js
+++ notes/dark-theme.js
@@ -1,8 +1,10 @@
 (function() {
   try {
-    localStorage.setItem('ls-theme-mode', '"dark"');
     document.documentElement.setAttribute('data-theme', 'dark');
+    document.documentElement.setAttribute('data-color', 'logseq');
     document.documentElement.classList.add('dark-theme');
-    document.documentElement.classList.remove('white-theme');
+    document.documentElement.classList.remove('white-theme', 'light-theme');
+    localStorage.setItem('ls-theme-mode', '"dark"');
+    localStorage.setItem('ls-theme-color', '"logseq"');
   } catch(e) {}
 })();
