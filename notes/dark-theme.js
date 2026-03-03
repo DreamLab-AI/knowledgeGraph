0e06d1a62c246ccd701e098301a1e560db89838f
jjohare <jjohare@users.noreply.github.com>
2026-03-03 20:01:53 +0000

    Deploy Logseq SPA @ c28cfdef308380e8e008f70fa9385832f866cfc5 jjohare/logseq@c28cfdef308380e8e008f70fa9385832f866cfc5

--- notes/dark-theme.js
+++ notes/dark-theme.js
@@ -2,7 +2,7 @@
   try {
     document.documentElement.setAttribute('data-theme', 'dark');
     document.documentElement.setAttribute('data-color', 'gunmetal');
-    document.documentElement.classList.add('dark-theme');
+    document.documentElement.classList.add('dark-theme', 'dark');
     document.documentElement.classList.remove('white-theme', 'light-theme');
     localStorage.setItem('ls-theme-mode', '"dark"');
     localStorage.setItem('ls-theme-color', '"gunmetal"');
